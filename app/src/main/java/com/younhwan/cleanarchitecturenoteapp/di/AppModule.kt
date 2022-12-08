e313642afb8faa7079da82deb2b5340e2f5e2fb5
younhwan97 <younhwan0903@gmail.com>
2022-12-08 16:35:11 +0900

    [Setting] Dagger Hilt

---
+++ app/src/main/java/com/younhwan/cleanarchitecturenoteapp/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.younhwan.cleanarchitecturenoteapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.younhwan.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDatabase
+import com.younhwan.cleanarchitecturenoteapp.feature_note.data.repository.NoteRepositoryImpl
+import com.younhwan.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import com.younhwan.cleanarchitecturenoteapp.feature_note.domain.use_case.DeleteNoteUseCase
+import com.younhwan.cleanarchitecturenoteapp.feature_note.domain.use_case.GetNotesUseCase
+import com.younhwan.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases{
+        return NoteUseCases(
+            getNotes = GetNotesUseCase(repository),
+            deleteNote = DeleteNoteUseCase(repository)
+        )
+    }
+}
