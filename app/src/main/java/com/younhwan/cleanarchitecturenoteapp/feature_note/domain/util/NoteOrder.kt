0de7159aa9e43e46d2c85fcb4ee228c3d915c345
younhwan97 <younhwan0903@gmail.com>
2022-12-08 17:41:26 +0900

    [Add] Radio btn/order section composable

--- app/src/main/java/com/younhwan/cleanarchitecturenoteapp/feature_note/domain/util/NoteOrder.kt
+++ app/src/main/java/com/younhwan/cleanarchitecturenoteapp/feature_note/domain/util/NoteOrder.kt
@@ -8,4 +8,12 @@ sealed class NoteOrder(val orderType: OrderType) {
 
     class Color(orderType: OrderType): NoteOrder(orderType)
 
+
+    fun copy(orderType: OrderType): NoteOrder {
+        return when(this){
+            is Title -> Title(orderType)
+            is Date -> Date(orderType)
+            is Color -> Color(orderType)
+        }
+    }
 }
