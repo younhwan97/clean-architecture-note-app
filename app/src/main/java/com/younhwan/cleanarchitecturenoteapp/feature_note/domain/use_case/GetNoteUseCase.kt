56f379cfbbb4902c623d52d314f02acc9ce06838
younhwan97 <younhwan0903@gmail.com>
2022-12-08 22:09:59 +0900

    [Add] GetNoteUseCase

---
+++ app/src/main/java/com/younhwan/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNoteUseCase.kt
@@ -0,0 +1,14 @@
+package com.younhwan.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.younhwan.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.younhwan.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class GetNoteUseCase(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(id: Int): Note? {
+        return repository.getNoteById(id)
+    }
+    
+}
